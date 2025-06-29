e21317daf7e5b175985c6fd72d97e721222ac8e2
unknown <qhliang1002@gmail.com>
2025-06-29 14:15:02 +0100

    draw triangles filled v2

--- Rasterizer/Rasterizer.cpp
+++ Rasterizer/Rasterizer.cpp
@@ -114,17 +114,11 @@ void drawModelTriangleFilled()
 		}
 		triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, white);
 	}
+	image.flip_vertically();
+	image.write_tga_file("output.tga");
 }
 int main(int argc, char** argv)
 {
-	TGAImage image(200, 200, TGAImage::RGB);
-	Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
-	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
-	triangle(t0[0], t0[1], t0[2], image, red);
-	triangle(t1[0], t1[1], t1[2], image, white);
-	triangle(t2[0], t2[1], t2[2], image, green);
-	image.flip_vertically();
-	image.write_tga_file("output.tga");
+	drawModelTriangleFilled();
 	return 0;
 }
