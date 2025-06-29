66b80cae2d95905ef8c78e8abfb2a8569d03a00d
unknown <qhliang1002@gmail.com>
2025-06-29 21:46:24 +0100

    zbuffer

--- Rasterizer/Rasterizer.cpp
+++ Rasterizer/Rasterizer.cpp
@@ -1,11 +1,13 @@
 // Rasterizer.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <limits>
 #include "tgaimage.h"
 #include "model.h"
 #include "geometry.h"
-#include <vector>
 #include <algorithm>
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -45,37 +47,66 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 	}
 }
 
+Vec3f barycentric(Vec3f& A, Vec3f& B, Vec3f& C, Vec3f& P)
+{
+	Vec3f s[2];
+	for (int i = 0; i < 2; i++)
+	{
+		s[i][0] = C[i] - A[i];
+		s[i][1] = B[i] - A[i];
+		s[i][2] = A[i] - P[i];
+	}
+
+	Vec3f u = s[0] ^ s[1];
+
+	if (std::abs(u[2]) > 1e-2)
+	{
+		return Vec3f(1.0f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+	}
+	return Vec3f(-1, 1, 1);
+}
+
 int getX(const Vec2i& a, const Vec2i& b, int y)
 {
 	if (b.y == a.y) return a.x;
 	return a.x + (b.x - a.x) * (y - a.y) / (b.y - a.y);
 }
 
-void triangle(const Vec2i &t0, const Vec2i& t1, const Vec2i& t2, TGAImage& image, TGAColor color)
+void triangle(Vec3f* pts, float* zbuffer, TGAImage& image, TGAColor color)
 {
-	std::vector<Vec2i> sortedVec = { t0,t1,t2 };
-	std::sort(sortedVec.begin(), sortedVec.end(), [](const Vec2i& v1, const Vec2i& v2)
-		{
-			return v1.y < v2.y;
-		});
+	Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+	Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+	Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
+	//确定三角形的边框
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 2; j++) {
+			bboxmin[j] = std::max(0.f, std::min(bboxmin[j], pts[i][j]));
+			bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+		}
+	}
 
-	for (int y = 0; y < sortedVec[2].y - sortedVec[0].y; y++)
+	Vec3f P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
 	{
-		int Ax = getX(sortedVec[0], sortedVec[2], y + sortedVec[0].y);
-		bool upperPart = y > (sortedVec[1].y - sortedVec[0].y) || sortedVec[1].y == sortedVec[0].y;
-		int Bx;
-		if (!upperPart)
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
 		{
-			Bx = getX(sortedVec[0], sortedVec[1], y + sortedVec[0].y);
-		}
-		else
-		{
-			Bx = getX(sortedVec[1], sortedVec[2], y + sortedVec[0].y);
-		}
-		if (Ax > Bx) std::swap(Ax, Bx);
-		for (int x = Ax; x < Bx; x++)
-		{
-			image.set(x, sortedVec[0].y + y, color);
+			Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
+
+			if (bc_screen[0] < 0.0f || bc_screen[1] < 0.0f || bc_screen[2] < 0.0f)
+				continue;
+
+			P.z = 0;
+			for (int i = 0; i < 3; i++)
+			{
+				P.z += pts[i][2] * bc_screen[i];
+			}
+
+			if (zbuffer[int(P.x + P.y * width)] < P.z)
+			{
+				zbuffer[int(P.x + P.y * width)] = P.z;
+				image.set(P.x, P.y, color);
+			}
+			
 		}
 	}
 }
@@ -102,17 +133,30 @@ void drawModelLine()
 
 }
 
+
+
+Vec3f world2screen(Vec3f v) {
+	return Vec3f(int((v.x + 1.) * width / 2. + .5), int((v.y + 1.) * height / 2. + .5), v.z);
+}
+
 void drawModelTriangleFilled()
 {
+	//add z buffer
+	float* zbuffer = new float[width * height];
+	for (int i = 0; i < width * height; i++)
+	{
+		zbuffer[i] = -std::numeric_limits<float>::max();
+	}
+
 	Model model("african_head.obj");
 	TGAImage image(width, height, TGAImage::RGB);
 	for (int i = 0; i < model.nfaces(); i++) {
 		std::vector<int> face = model.face(i);
-		Vec2i screen_coords[3];
+		Vec3f screen_coords[3];
 		Vec3f world_coords[3];
 		for (int j = 0; j < 3; j++) {
 			Vec3f v = model.vert(face[j]);
-			screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+			screen_coords[j] = world2screen(v);
 			world_coords[j] = v;
 		}
 		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
@@ -120,14 +164,17 @@ void drawModelTriangleFilled()
 		float intensity = n * light_dir;
 		if (intensity > 0) 
 		{
-			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(255 * intensity, 255 * intensity, 255 * intensity, 255));
+			triangle(screen_coords, zbuffer, image, TGAColor(255 * intensity, 255 * intensity, 255 * intensity, 255));
 		}
 	}
 	image.flip_vertically();
 	image.write_tga_file("output.tga");
 }
+
+
 int main(int argc, char** argv)
 {
+
 	drawModelTriangleFilled();
 	return 0;
 }
